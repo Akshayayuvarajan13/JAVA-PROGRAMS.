72563c208af5123a9794f7a8ea4f2dfcf0902e4c
Akshaya Abi <akshayayuvarajan13@gmail.com>
2026-09-18 16:20:34 +0530

    Added Java programs

--- LargestStack.java
+++ LargestStack.java
@@ -6,13 +6,14 @@ public class LargestStack {
         Stack<Integer> stack = new Stack<>();
         System.out.print("Enter number of elements:");
         int n = sc.nextInt();
-        System.out.println("Enter" + n +"elements");
-        for(int i = 0; i<n; i++){
+        System.out.println("Enter" + n + "elements");
+        for(int i = 0; i < n; i++){
             stack.push(sc.nextInt());
         }
 int largest = stack.get(0);
 for(int i = 1; i < stack.size();i++){
     if(stack.get(i)>largest){
+        largest = stack.get(i);
     }
 }
 System.out.println("Stack:" +stack);
