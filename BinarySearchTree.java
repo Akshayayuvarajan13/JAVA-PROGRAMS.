16e57152282344501879c9774f031a90c1716421
Akshaya Abi <akshayayuvarajan13@gmail.com>
2026-09-22 14:31:05 +0530

    Added Java programs

---
+++ BinarySearchTree.java
@@ -0,0 +1,63 @@
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node (int data){
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+public class BinarySearchTree{
+    static Node insert(Node root,int value){
+        if(root == null){
+            return new Node(value);
+        }
+        if(value< root.data){
+            root.left=insert(root.left,value);
+        }
+        else{
+            root.right= insert(root.right,value);
+        }
+        return root;
+    }
+    static boolean search(Node root,int value){
+        if(root == null){
+            return false;
+        }
+        if(root.data == value){
+            return true;
+        }
+        if(value<root.data){
+            return search(root.left,value);
+        }
+        return search (root.right,value);
+    }
+    static void inorder(Node root){
+        if(root==null){
+            return;
+        }
+        inorder(root.left);
+        System.out.println(root.data+"");
+        inorder(root.right);
+    }
+    public static void main(String[] args) {
+        Node root = null;
+        root = insert(root,50);
+        root = insert(root,30);
+        root=insert(root,70);
+        root = insert(root,20);
+        root = insert(root,40);
+        root = insert(root,60);
+        root = insert(root,80);
+        System.out.println("Inorder");
+        inorder(root);
+        int value = 60;
+        if(search(root,value)){
+            System.out.println("\n"+value+"found");
+        }
+        else{
+            System.out.println("\n"+value+"not found");
+        }
+}
+}
